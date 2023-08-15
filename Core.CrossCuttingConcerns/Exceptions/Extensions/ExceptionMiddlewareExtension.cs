95ad7dea1e2fd86134d1eb399118b8bc8436c597
alikemaluysal <alikemaluysal4235@gmail.com>
2023-08-15 18:07:40 +0300

    feat(core.persistence) global exception handler implemented

---
+++ Core.CrossCuttingConcerns/Exceptions/Extensions/ExceptionMiddlewareExtension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Exceptions.Extensions;
+
+public static class ExceptionMiddlewareExtension
+{
+
+    public static void ConfigureCustomExcetionMiddleware(this IApplicationBuilder app) => app.UseMiddleware<ExceptionMiddleware>();
+}
---
+++ Core.CrossCuttingConcerns/Exceptions/HttpProblemDetails/InternalServerErrorDetails.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core.CrossCuttingConcerns.Exceptions.HttpProblemDetails;
+
+public class InternalServerErrorDetails : ProblemDetails
+{
+    public InternalServerErrorDetails(string details)
+    {
+        Title = "Internal Server Error";
+        Detail = "Internal Server Error";
+        Status = StatusCodes.Status500InternalServerError;
+        Type = "https://alikemaluysal.com/probs/internal";
+    }
+
+}
+
